f59eafd38fcb5be4d5948971f753428d23054aaf
Eric Defore <eric.defore@gmail.com>
2016-06-27 11:14:11 -0400

    CSS Fixes after converting to a Child Theme. Referencing #2

--- forms/sponsor_submit.php
+++ forms/sponsor_submit.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $text = $_REQUEST['msg'];
